85dabee9b779af2faeabac1210a5868e39d142cb
anshmahajan26 <anshmahajan678@gmail.com>
2026-02-16 07:21:46 +0530

    react component

--- REACT/first/src/Component/first.jsx
+++ REACT/first/src/Component/first.jsx
@@ -4,6 +4,10 @@ function Card({name}){
     return(
         <>
         <h1>Hiii {name}</h1>
+        <h2> Here is my first react component </h2>
+        <div>
+            
+        </div>
         </>
     )
 }
