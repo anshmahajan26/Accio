f4951a0bb12e1959749c844079cb3284b3fc4b17
anshmahajan26 <anshmahajan678@gmail.com>
2026-04-19 11:47:14 +0530

    done

--- REACT/WeatherProj/WeatherReact/src/App.jsx
+++ REACT/WeatherProj/WeatherReact/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
+import Footer from './component/Footer'
+import Header from './component/Header'
+import Search from './component/Search'
+import WeatherCard from './component/WeatherCard'
 
 function App() {
   
 
   return (
     <>
-      
-      
+      <Header/>
+      <Search/>
+      <WeatherCard/>
+      <Footer/>
     </>
   )
 }
