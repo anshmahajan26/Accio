534701dd5238e1caf9ac493e4e55762d8d09b8f4
anshmahajan26 <anshmahajan678@gmail.com>
2026-07-22 14:35:04 +0530

    done

--- Node/app.js
+++ Node/app.js
@@ -17,3 +17,43 @@ const server = http.createServer((req, res) => {
         `);
 
     }
+     else if (req.url === "/add") {
+
+        let ans = 10 + 20;
+
+        res.end(`<h1>Addition = ${ans}</h1>`);
+
+    }
+
+    else if (req.url === "/sub") {
+
+        let ans = 20 - 10;
+
+        res.end(`<h1>Subtraction = ${ans}</h1>`);
+
+    }
+
+    else if (req.url === "/mul") {
+
+        let ans = 10 * 20;
+
+        res.end(`<h1>Multiplication = ${ans}</h1>`);
+
+    }
+
+    else if (req.url === "/div") {
+
+        let ans = 20 / 10;
+
+        res.end(`<h1>Division = ${ans}</h1>`);
+
+    }
+
+    else {
+
+        res.statusCode = 404;
+        res.end("<h1>Page Not Found</h1>");
+
+    }
+
+});
