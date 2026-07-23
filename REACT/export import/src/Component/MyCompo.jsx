1335d362ed2336d98af8b91f9c010f2b85df2261
anshmahajan26 <anshmahajan678@gmail.com>
2026-07-23 10:50:58 +0530

    done

--- REACT/export import/src/Component/MyCompo.jsx
+++ REACT/export import/src/Component/MyCompo.jsx
@@ -1,9 +1,10 @@
-function MyCompo(){
-
-    return(
-    <>
-
-    </>
-    )
+function MyCompo() {
+  return (
+    <div>
+      <h2>My Component</h2>
+      <p>This is my component rendering in the browser.</p>
+    </div>
+  );
 }
+
 export default MyCompo;
