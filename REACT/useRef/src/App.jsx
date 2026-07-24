4bd4c4ac33248ff87688139bc7be5cc9f56b2b17
anshmahajan26 <anshmahajan678@gmail.com>
2026-07-25 00:48:17 +0530

    done with useRef()

--- REACT/useRef/src/App.jsx
+++ REACT/useRef/src/App.jsx
@@ -1,11 +1,14 @@
-
+import { useRef } from "react"
 
 function App() {
-
-
+const inputRef = useRef(null);
+function handleFocus(){
+  inputRef.current.focus();
+};
   return (
     <>
-      
+      <input ref={inputRef} type="text"></input>
+      <button onClick={handleFocus}>focus on input box</button>
     </>
   )
 }
