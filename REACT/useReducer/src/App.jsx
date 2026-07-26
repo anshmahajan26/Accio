a6ca80dc8c07e588b37caf2cf950b1f8bec7e415
anshmahajan26 <anshmahajan678@gmail.com>
2026-07-26 22:29:12 +0530

    done reducer

--- REACT/useReducer/src/App.jsx
+++ REACT/useReducer/src/App.jsx
@@ -1,11 +1,30 @@
+import { useReducer } from "react"
+const intialState =0;
 
+function reducer(state,action){
+  switch(action.type){
+    case "INCREMENT":
+      return state+1;
+    case "DECREMENT":
+      return state -1;
+    case "RESET":
+      return 0;
+    default:
+      return state;
+  }
+}
 
 function App() {
-
+const [count,dispatch] = useReducer(reducer,intialState);
 
   return (
     <>
-     
+    <div>
+     <h1>{count}</h1>
+     <button onClick={()=>dispatch({type:"INCREMENT"})}>+</button>
+    <button onClick = {()=>dispatch({type:"DECREMENT"})}>-</button>
+    <button onClick={()=>dispatch({type:"RESET"})}>RESET</button>
+    </div>
     </>
   )
 }
