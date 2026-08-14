50ee92f57df85d26342a27eea6a44d1c68538e11
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-14 17:29:38 +0530

    done

--- ACCIO Practice/counter/backend/Server.js
+++ ACCIO Practice/counter/backend/Server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+//mongoSchema
+const counter = new mongoose.Schema({
+    count: Integer,
+})
+const Counter = mongoose.model("Counter",counter);
+//mongo connection
+mongoose.connect("mongodb://localhost:27017/new1");
+
 
 app.get("/",(req,res)=>{
     res.send("hello");
