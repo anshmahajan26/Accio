81ebea1a3af1525136b615d308d8b3f0f7df41ca
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-14 20:58:38 +0530

    noow connecting fronend with bakcned

--- ACCIO Practice/counter/backend/Server.js
+++ ACCIO Practice/counter/backend/Server.js
@@ -3,17 +3,50 @@ const app = express();
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
+//middelware to handle http request data
+app.use(express.json());
 //mongoSchema
 const counter = new mongoose.Schema({
-    count: Integer,
+    count: Number,
+    name:String
 })
 const Counter = mongoose.model("Counter",counter);
 //mongo connection
-mongoose.connect("mongodb://localhost:27017/new1");
+//! we can add it using async function also
+async function dbConnect(){
+ try{
+    await mongoose.connect("mongodb://localhost:27017/new1");
+    console.log("DB connected");
+ }catch(e){
+    console.log("DB connection issue");
+ }
+}
+dbConnect();
 
 
-app.get("/",(req,res)=>{
-    res.send("hello");
+app.post("/",async (req,res)=>{
+    const cdb = await Counter.create(req.body);
+    res.send(cdb);
+    
+});
+app.get("/history", async (req, res) => {
+    const gdb = await Counter.find();
+    res.send(gdb);
+});
+app.delete("/history/:id",async(req,res)=>{
+    try{
+        const id = req.params.id;
+         const ddb = await Counter.findByIdAndDelete(id);
+          if (!ddb) {
+            return res.status(404).send("Counter not found");
+        }
+          res.send("deleted");
+
+    }catch(e){
+        console.log(e);
+    }
+    
+   
 });
 
 app.listen("8080",()=>{
