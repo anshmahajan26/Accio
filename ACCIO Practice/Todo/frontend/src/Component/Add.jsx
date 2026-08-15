4ecbd56c37a1d5850ee943e7bacf3fb5c8be7785
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-15 12:23:52 +0530

    donw

--- ACCIO Practice/Todo/frontend/src/Component/Add.jsx
+++ ACCIO Practice/Todo/frontend/src/Component/Add.jsx
@@ -2,32 +2,32 @@ import React from "react"
 import { useState } from "react";
 
 function Add() {
-//to handle task in input filed this hook is used
-const [task,setTask]= useState("");
-//this is store input field task and display using map
-const [show,setShow]= useState([]);
+    //to handle task in input filed this hook is used
+    const [task, setTask] = useState("");
+    //this is store input field task and display using map
+    const [show, setShow] = useState([]);
 
-//!input value handler
-const inputVal = (e)=>{
-    setTask(e.target.value);
-}
-//!form event handler and submit button handler.
-const handeSubmit = (e)=>{
-    e.preventDefault();
-    
-    if(!task){
-        alert("put task in input field first");
-    }else{
-         setShow([...show,task]);
-        setTask("");
-    } 
-}
-//! Delete button handler
-const onDel = (index)=>{
-    setShow(show.filter((item, i)=>{
-       return i !== index;
-    }));
-}
+    //!input value handler
+    const inputVal = (e) => {
+        setTask(e.target.value);
+    }
+    //!form event handler and submit button handler.
+    const handeSubmit = (e) => {
+        e.preventDefault();
+
+        if (!task) {
+            alert("put task in input field first");
+        } else {
+            setShow([...show, task]);
+            setTask("");
+        }
+    }
+    //! Delete button handler
+    const onDel = (index) => {
+        setShow(show.filter((item, i) => {
+            return i !== index;
+        }));
+    }
     return (
         <>
             <h1>ADD TASK</h1>
@@ -37,17 +37,18 @@ const onDel = (index)=>{
                 <button type="submit" >ADD TASK</button>
             </form>
             <br></br>
-             <br></br>
-              <br></br>
+            <br></br>
+            <br></br>
             <h1>TASK TO COMPLETE</h1>
             <ul>{
-                show.map((item,index)=>(
+                show.map((item, index) => (
                     <li key={index}>
-                        {item} <button onClick={()=>onDel(index)}>Delete</button>
+                        {item} <button onClick={() => onDel(index)}>Delete</button>
+                        <button>Edit</button>
                     </li>
                 ))
-                }</ul>
-             
+            }</ul>
+
 
         </>
     )
