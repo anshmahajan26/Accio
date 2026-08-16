a3889c1eb5bc12ab5c12f22537188da903cc9247
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-16 17:51:41 +0530

    donw with hide passowrkd

--- ACCIO Practice/passwordHide/frontend/src/App.jsx
+++ ACCIO Practice/passwordHide/frontend/src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react"
 
 function App() {
-  
+const [pass, setPass]= useState("");
+const [show, setShow] = useState(false);
+const handleInput = (event)=>{
+  setPass(event.target.value);
+}
+const showclick = ()=>{
+ setShow(true);
+}
+const hideclick = ()=>{
+  setShow(false);
+}
+let inputType = "password"
+if(show){
+  inputType = "text";
+}
+
   return (
     <>
-      
+      <input name="pass" value={pass} type={inputType} onChange={handleInput}></input>
+      <button onClick={showclick}>show</button>
+      <button onClick={hideclick}>hide</button>
     </>
   )
 }
