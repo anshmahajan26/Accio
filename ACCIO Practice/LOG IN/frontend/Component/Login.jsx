c09879fa2a1be8afc92b75c45567d43d126fc004
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-18 18:26:40 +0530

    login form start

--- ACCIO Practice/LOG IN/frontend/Component/Login.jsx
+++ ACCIO Practice/LOG IN/frontend/Component/Login.jsx
@@ -1,35 +1,43 @@
 import { useState } from "react"
-function Login(){
-    const [email, setEmail]= useState("");
-    const [ pass, setPass]= useState("");
-   const[user,setUser]= useState({
-    email:"",
-    password:""
-   });
-   const setEm=(event)=>{
-    setEmail(event.target.value);
-   }
-   const setPasss=(event)=>{
-    setPass(event.target.value);
-   }
-const handlesubmit=(e)=>{
-    e.preventDefault();
-    setUser({
-        email:email,
-        password:pass
+import { useNavigate } from "react-router-dom";
+import Show from "./Show";
+function Login() {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const navigate = useNavigate();
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
     });
-    console.log(user);
-}
+    const setEm = (event) => {
+        setEmail(event.target.value);
+    }
+    const setPasss = (event) => {
+        setPass(event.target.value);
+    }
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        setUser({
+            email: email,
+            password: pass
+        });
+        // console.log(user);
+        
+        navigate("/Show");
 
-    return(
+
+    }
+
+    return (
         <>
-        <form method="post" onSubmit={handlesubmit}>
-            <label htmlFor="">email</label>
-            <input type="text" value={email} name="email" onChange={setEm}></input>
-            <label>Passoword</label>
-            <input type="password" value={pass} name="password" onChange={setPasss}></input>
-            <button type="submit">submit</button>
-        </form>
+            <form method="post" onSubmit={handlesubmit}>
+                <label htmlFor="">email</label>
+                <input type="text" value={email} name="email" onChange={setEm}></input>
+                <label>Passoword</label>
+                <input type="password" value={pass} name="password" onChange={setPasss}></input>
+
+                <button type="submit">submit</button>
+            </form>
         </>
     )
 }
