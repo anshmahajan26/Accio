61e4391bdd7de64cf3ebc0a22b940e823edc3b2c
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-18 18:05:03 +0530

    login form start

--- ACCIO Practice/LOG IN/frontend/Component/Login.jsx
+++ ACCIO Practice/LOG IN/frontend/Component/Login.jsx
@@ -6,15 +6,27 @@ function Login(){
     email:"",
     password:""
    });
-
+   const setEm=(event)=>{
+    setEmail(event.target.value);
+   }
+   const setPass=(event)=>{
+    setPass(event.target.value);
+   }
+const handlesubmit=(e)=>{
+    e.preventDefault();
+    setUser({
+        email:email,
+        password:pass
+    });
+}
 
     return(
         <>
         <form method="post" onSubmit={}>
             <label htmlFor="">email</label>
-            <input type="text" value={email} name="email" onChange={}></input>
+            <input type="text" value={email} name="email" onChange={setEm}></input>
             <label>Passoword</label>
-            <input type="password" value={pass} name="password" onChange={}></input>
+            <input type="password" value={pass} name="password" onChange={setPass}></input>
             <button type="submit">submit</button>
         </form>
         </>
