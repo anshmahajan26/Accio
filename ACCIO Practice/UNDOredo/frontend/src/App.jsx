f0ad4ccde5cd917a48efe8127b3699f2b164904b
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-19 14:53:27 +0530

    start with login bakcen

--- ACCIO Practice/UNDOredo/frontend/src/App.jsx
+++ ACCIO Practice/UNDOredo/frontend/src/App.jsx
@@ -1,11 +1,22 @@
-
+import { useState } from "react"
 
 function App() {
-  
+  const [input,setInput]= useState("");
+  const [history,setHistory]= useState([]);
+  const[future,setFuture]= useState([]);
+
+  const handleChange= (event)=>{
+     const newValue = e.target.value;
+     setHistory([...history, text]);
+     setText(newValue);
+    // setFuture([]);
+
+  };
 
   return (
     <>
-     
+     <h1>UNDO REDU</h1>
+     <input type="text" onChange={handleChange}></input>
     </>
   )
 }
