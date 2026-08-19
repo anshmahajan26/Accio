276be8e98cfacca54c595af67565e401031d082a
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-19 13:38:16 +0530

    start with login bakcen

---
+++ ACCIO Practice/LOG IN/backend/Server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(express.json());
+
+const user = new mongoose.Schema({
+    username:String,
+    password: String
+});
+const Logger = mongoose.model("Logger",user);
+
+app.post("/",async(req,res)=>{
+    try{
+    const udb =await Logger.create(req.body);
+    res.send(udb);
+    }catch(e){
+        console.log("Server error");
+    }
+});
+
+app.listen("8080",()=>{
+    console.log("you are on port 8080");
+})
