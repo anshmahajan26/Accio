b0d93e578ac2dc852aa7d44097b7ef35ea0d281a
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-23 07:02:57 +0530

    donw with server

---
+++ ACCIO Practice/UNDOredo/backend/Server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongooose");
+const cors = require ("cors");
+
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen ("8080",()=>{
+    console.log("Yes i am listening");
+})
