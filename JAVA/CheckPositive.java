e9b1a0a4ec2dc266c4eb2d19d1bf3d13645232e8
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-25 12:55:31 +0530

    first coede done

---
+++ JAVA/CheckPositive.java
@@ -0,0 +1,14 @@
+//!check Pos or neg
+class CheckPositive{
+    function check(){
+        int num = 10;
+        if(num>=0){
+            System.out.println("postive");
+        }else{
+            System.out.println("Negative");
+        }
+    }
+    public static void main (String args[]){
+        check();
+    }
+}
