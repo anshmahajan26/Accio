814e7d9c7ef9b9105aa35fed0ef850b43aaa6345
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-26 13:21:27 +0530

    donw

--- JAVA/Answers_Practice.java
+++ JAVA/Answers_Practice.java
@@ -4,11 +4,35 @@
         int a = -7,b=0,c=7,d=153,e=37473;
         int []arr={2,0,7,4,3,9,88};
         int arr1[]={3,7,14,31,49,100};
-        
+
         Practice o = new Practice();
         //Q1
         o.check();
         //Q2
         o.evenOdd(a);
+        //Q3
+        o.sumNo(arr);
+        //Q4
+        o.max3(a,b,c);
+        //extra
+        int var =o.maxAll(4,63,6,325,53,9,-4,-30);
+        System.out.println(var+"  is maximum among all");
+        //Q5
+        o.LeapYear(2013);
+        //Q6
+        System.out.println(o.isPrime(4));
+        //Q7
+        System.out.println(o.PrimeCount(2,4,7,2,6,13,19));
+
+        //Q8
+        System.out.println(o.sumofDigi(122));
+        //Q9
+        System.out.println(o.Rev(123));
+
+
+        //Practice o2 = new Practice();
+    //     int abc=Practice.maxAll(3,21,4,64,1,4,-1,-1999);
+    // System.out.println(abc);
     }
+
 }
--- JAVA/Practice.java
+++ JAVA/Practice.java
@@ -1,19 +1,112 @@
 //!check Pos or neg
  public class Practice{
    public void check(){
-        int num = 10;
-        if(num>=0){
+        int num = 0;
+        if (num==0){
+            System.out.println("zero is neither +ve  nor -ve");
+        }
+        else if(num>0){
             System.out.println("positive");
         }else{
             System.out.println("Negative");
         }
     }
 
-    public static void evenOdd(int num){
+    public void evenOdd(int num){
+      
         if(num%2==0){
             System.out.println("even");
         }else{
              System.out.println("odd");
         }
     }
+
+    public  void sumNo(int arr[]){
+        int sum =0;
+        for(int i =0; i<arr.length; i++){
+            sum+= arr[i];
+        }
+        System.out.println(sum);
+    }
+
+    public  void max3(int num1,int num2,int num3){
+        if((num1>num2)&& (num1>num3)){
+            System.out.println("num1 is max");
+        }
+        else if((num2>num1) && (num2>num3)){
+             System.out.println("num2 is max");
+        }
+        else {
+             System.out.println("num3 is max");
+        }
+    }
+
+    public  int maxAll(int...numbers){
+        int max = numbers[0];
+        for(int i=1; i<numbers.length; i++){
+            if(numbers[i]>max){
+                max=numbers[i];
+            }
+        }
+        return max;
+    }
+
+    public  void LeapYear(int year){
+        if(year%400==0){
+            System.out.println("leap year");
+        }else if(year % 4 ==0 && year % 100!=0){
+            System.out.println("leap year");
+        }else{
+            System.out.println("NOT leap year");
+        }
+    }
+
+    public  boolean isPrime(int num){
+        boolean prime = true;
+      if(num == 1){
+        return false;
+      }else if(num ==2){
+        return prime;
+      }else{
+
+      for(int i =2; i<=(num/2); i++){
+        if(num % i==0){
+            prime= false;
+            break;
+        }
+        }
+      }
+      return prime;
+    }
+
+    public int PrimeCount(int...number){
+        int count =0;
+        for(int i =0; i<number.length; i++){
+            if(isPrime(number[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+    public int sumofDigi(int num){
+        int sum =0;
+        while(num>0){
+            int digiLast = num % 10;
+            num = num/10;
+            sum+=digiLast;
+        }
+        return sum;
+    }
+
+    public int Rev(int num){
+        int rev = 0, rem;
+        while(num>0){
+            rem = num%10;
+            rev = rev*10+rem;
+            num = num/10;
+        }
+        return rev;
+    }
+
+
 }
