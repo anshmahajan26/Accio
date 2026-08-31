8302b3fba90720497cb599f3c2ab704e2d6bf879
anshmahajan26 <anshmahajan678@gmail.com>
2026-08-31 17:04:47 +0530

    mock prepare

--- ACCIO Practice/UNDOredo/frontend/src/Component/New.jsx
+++ ACCIO Practice/UNDOredo/frontend/src/Component/New.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 
 function New(){
+    const [input, setInput]= useState("");
+    const [history, setHistory]= useState([]);
+    const [future, setFuture]= useState([]);
+
+    const handleInput = (event)=>{
+        const newVal = event.target.value;
+        setInput(newVal);
+        setHistory([...history, input]);
+
+    }
+    const handleUndo = ()=>{
+        const prevVal = history[history.length -1];
+        setFuture(input,...future);
+    }
 
     return(
         <>
-        <input type="text"onChange={as}></input>
+        <input type="text" ></input>
         </>
     )
 }
